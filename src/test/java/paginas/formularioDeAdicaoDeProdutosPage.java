085ebb82598e7ebaba95428a3d9532eeb68d52f5
Cristiane Abreu <abreu.kr@gmail.com>
2022-02-21 20:51:14 +0000

    Criei a página de fomulário de adição de produtos seguindo o padrão Page Objects.

---
+++ src/test/java/paginas/formularioDeAdicaoDeProdutosPage.java
@@ -0,0 +1,11 @@
+package paginas;
+
+import org.openqa.selenium.WebDriver;
+
+public class formularioDeAdicaoDeProdutosPage {
+    private WebDriver navegador;
+
+    public formularioDeAdicaoDeProdutosPage(WebDriver navegador) {
+        this.navegador = navegador;
+    }
+}
